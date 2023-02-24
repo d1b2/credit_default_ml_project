5488bd644b7a9bfcda934fee88a8d6b5a5bd19c9
Dhruv Bakshi <dhruvbakshi20@gmail.com>
2023-02-24 15:11:43 +0530

    mlflow secrets added

--- src/Credit_Default/constants/__init__.py
+++ src/Credit_Default/constants/__init__.py
@@ -3,6 +3,7 @@ from pathlib import Path
 CONFIG_FILE_PATH = Path("configs/config.yaml")
 PARAMS_FILE_PATH = Path("params.yaml")
 SCHEMA_FILE_PATH=Path("configs/schema.yaml")
+SECRETS_FILE_PATH = Path("configs/secrets.yaml")
 TRAIN_FILE_PATH=Path("artifacts/data_ingestion/train_test/train/train.csv")
 TEST_FILE_PATH=Path("artifacts/data_ingestion/train_test/test/test.csv")
 SCHEMA_CLEAN_FILE_PATH=Path("configs/schema_clean.yaml")
